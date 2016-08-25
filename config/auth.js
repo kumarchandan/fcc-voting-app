7355306fba8ef2d16fc2040de245ec188dda67b4
K.Chan <chandankumar99341@gmail.com>
2016-08-25 23:22:59 +0530

    remove custom option for unauthorize user

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,7 @@ module.exports = {
     'twitterAuth': {
         'consumerKey': 'CqAGC09OxTRLGyT16LRomoqAR',
         'consumerSecret': 'ENcXEC8MjQ3y2s3JXYUut3Te01BXk4d1XaMOG7CLDnoIK9ftg2',
-        // 'callbackURL': 'http://127.0.0.1:3000/auth/twitter/callback'
-        'callbackURL': 'https://kchan-voting-app.herokuapp.com/auth/twitter/callback'
+        'callbackURL': 'http://127.0.0.1:3000/auth/twitter/callback'
+        // 'callbackURL': 'https://kchan-voting-app.herokuapp.com/auth/twitter/callback'
     }
 }
