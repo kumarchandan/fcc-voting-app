18c6e1f3da05af6e25200f17882c7e59b15741c4
K.Chan <chandankumar99341@gmail.com>
2016-08-25 23:31:22 +0530

    cleanup

--- config/database.js
+++
@@ -1,6 +0,0 @@
-// database.js
-
-module.exports = {
-    // mongoURL: 'mongodb://localhost:27017/voting-app' || 'mongodb://kchan:99341@ds013926.mlab.com:13926/voting-app'
-    mongoURL: 'mongodb://kchan:99341@ds013926.mlab.com:13926/voting-app'
-}
